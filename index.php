94953660c6302b8c3d8f3631839849538d8741a9
dyxs <dyxs@mail.ru>
2015-05-25 17:30:03 +0300

    message

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Lagunov
- * Date: 20.05.2015
- * Time: 23:28
- */
+for ($i=0;$i<20;$i++) {
+    $mas[] = rand(0, 100);
+    print $mas[$i];
+    print"<br>";
+
+}
+
+foreach( $mas as $key => $value){
+    $max[0]=$mas[0];
+    isset($mas[$key + 1]);
+    if ($max<$mas[$key + 1]){
+        echo "a is bigger than b";
+
+    }
+}
