ad9f0d6faf6804871afe43d4603f25f7597534b9
dyxs@mail.ru <dyxs@mail.ru>
2015-05-31 22:50:26 +0300

    nerabotaet fibonachi

---
+++ fibonachi.php
@@ -0,0 +1,24 @@
+<?php
+
+function fib($num)
+{
+    if ($num = 0) {
+        return 0;
+    }
+    if ($num = 1) {
+        return 1;
+    }
+    if ($num = 2) {
+        return 1;
+    }
+
+    else {
+        return fib($num-1) + fib($num-2);
+    }
+}
+
+for ($num = 1; $num <= 16; $num++) {
+    echo(fib($num));
+}
+
+?>
